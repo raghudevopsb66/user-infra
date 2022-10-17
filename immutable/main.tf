39e122a297fcddbc9717d4bce8cdb6b30531de6d
Raghu K <raghuk.vit@gmail.com>
2022-10-17 08:36:50 +0530

    Update infra/user-infra/

--- immutable/main.tf
+++ immutable/main.tf
@@ -3,7 +3,7 @@ module "user" {
   COMPONENT        = var.COMPONENT
   ENV              = var.ENV
   APP_PORT         = 8080
-  LB_RULE_PRIORITY = 2
+  LB_RULE_PRIORITY = 6
   APP_VERSION      = var.APP_VERSION
   desired_capacity = var.desired_capacity
   max_size         = var.max_size
